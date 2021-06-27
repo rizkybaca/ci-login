17f4b39f504824c2be9b53d9bf1b6e0b8fae558f
ta <akun.rizky.baca@gmail.com>
2021-06-28 00:51:50 +0700

    menu management

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -12,35 +12,48 @@
             <!-- Divider -->
             <hr class="sidebar-divider">
 
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Admin
-            </div>
+            <!-- get menu -->
+            <?php 
+              $role_id=$this->session->userdata('role_id');
+              $q_menu="SELECT `user_menu`.`id`, `menu`
+                        FROM `user_menu` JOIN `user_access_menu` 
+                        ON `user_menu`.`id` = `user_access_menu`.`menu_id`
+                        WHERE `user_access_menu`.`role_id` = $role_id
+                        ORDER BY `user_access_menu`.`menu_id` ASC
+                      ";
+              $menu=$this->db->query($q_menu)->result_array();
+            ?>
 
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
-                <a class="nav-link" href="index.html">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
-            </li>
+            <!-- loop menu -->
+            <?php foreach ($menu as $m) : ?>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider">
+              <!-- Heading -->
+              <div class="sidebar-heading">
+                <?= $m['menu']; ?>
+              </div>
 
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                User
-            </div>
+              <!-- get sub-menu -->
+              <?php 
+                $menu_id=$m['id'];
+                $q_sub="SELECT * FROM `user_sub_menu` WHERE `menu_id` = $menu_id AND `is_active` = 1";
+                $sub=$this->db->query($q_sub)->result_array();
+              ?>
 
-            <!-- Nav Item - Profile -->
-            <li class="nav-item">
-                <a class="nav-link" href="charts.html">
-                    <i class="fas fa-fw fa-user"></i>
-                    <span>My Profile</span></a>
-            </li>
+              <?php foreach ($sub as $s) : ?>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider">
+                <!-- Nav Item - Dashboard -->
+                <li class="nav-item">
+                  <a class="nav-link" href="<?= base_url($s['url']);  ?>">
+                    <i class="<?= $s['icon']; ?>"></i>
+                    <span><?= $s['title']; ?></span></a>
+                </li>
+
+              <?php endforeach; ?>
+
+              <!-- Divider -->
+              <hr class="sidebar-divider">
+
+            <?php endforeach; ?>
 
             <!-- Nav Item - Logout -->
             <li class="nav-item">
@@ -58,4 +71,4 @@
             </div>
 
         </ul>
-        <!-- End of Sidebar -->
+        <!-- End of Sidebar
