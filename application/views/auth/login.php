8c9b9536871b1664871bbeecff0130750f44eed1
ta <akun.rizky.baca@gmail.com>
2021-06-27 18:27:29 +0700

    regist

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -10,6 +10,7 @@
                 <div class="p-5">
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Login Page</h1>
+                    <?= $this->session->flashdata('message'); ?>
                   </div>
                   <form class="user">
                     <div class="form-group">
